1bc1f4140ac94fdd17265eb207d5a59706052cd2
Dan K <danielkermode@hotmail.co.nz>
2015-12-26 22:36:35 +0000

    added select input

--- components/DisplayBox.jsx
+++ components/DisplayBox.jsx
@@ -1,4 +1,59 @@
 import React from 'react';
+import FilteredMultiSelect from 'react-filtered-multiselect';
+
+let CULTURE_SHIPS = [
+  {id: 1, name: '5*Gelish-Oplule'},
+  {id: 2, name: '7*Uagren'},
+// ...
+  {id: 249, name: 'Zero Gravitas'},
+  {id: 250, name: 'Zoologist'}
+]
+
+const MySelect = React.createClass({
+
+  getInitialState() {
+    return {selectedShips: []}
+  },
+
+  handleDeselect(index) {
+    let selectedShips = this.state.selectedShips.slice()
+    selectedShips.splice(index, 1)
+    this.setState({selectedShips})
+  },
+  handleSelectionChange(selectedShips) {
+    this.setState({selectedShips})
+  },
+  render() {
+    let {selectedShips} = this.state
+    return <div style={{width: '20%'}}>
+      <br/>
+      <FilteredMultiSelect
+        onChange={this.handleSelectionChange}
+        options={CULTURE_SHIPS}
+        selectedOptions={selectedShips}
+        textProp="name"
+        valueProp="id"
+        classNames={
+			   { button: 'btn btn-sm btn-block btn-warning glyphicon glyphicon-plus',
+			   	 buttonActive: 'FilteredMultiSelect__button--active',
+			   	 filter: 'form-control',
+			   	 select: 'form-control'}
+			  }
+		size='3'
+		buttonText=" Add Hint"
+		/>
+      {selectedShips.length === 0 && <p>(Select a hint for this paragraph.)</p>}
+      {selectedShips.length > 0 && <ul>
+        {selectedShips.map((ship, i) => <li key={ship.id}>
+          {`${ship.name} `}
+          <button type="button" onClick={this.handleDeselect.bind(null, i)}>
+            &times;
+          </button>
+        </li>)}
+      </ul>}
+    </div>
+  }
+})
 
 export const DisplayBox = React.createClass({
 
@@ -16,6 +71,9 @@ export const DisplayBox = React.createClass({
 					<button style={{margin: '0.2em'}} className="btn btn-danger glyphicon glyphicon-trash"
 						onClick={this.props.onClickRemoveHandler}
 					> Remove</button>
+				</div>
+				<div>
+					<MySelect/>
 				</div>	
 			</div>
 		);
