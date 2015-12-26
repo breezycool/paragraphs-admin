d7edec75df1906f856dbc1e0b1a352229b2d4198
breezycool <lachlankermode@live.com>
2015-12-26 17:20:05 +0000

    added redux tools

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,23 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { reducer } from './reducer'
+import DevTools from './devtools'
 
-export let store = createStore(reducer)
+const createFinalStore = compose(
+	DevTools.instrument()
+)(createStore)
+
+export function configureStore(initialState) {
+  const store = createFinalStore(reducer);
+
+  // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
+  // if (module.hot) {
+  //   module.hot.accept('../reducers', () =>
+  //     store.replaceReducer(require('../reducers')/*.default if you use Babel 6+ */)
+  //   );
+  // }
+
+  return store;
+}
 
 
 
