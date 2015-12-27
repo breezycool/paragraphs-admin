f7eca77fd1a7ec15b078b21069b1abc09d164762
Daniel Kermode <Daniel Kermode>
2015-12-27 10:41:06 +0000

    added multiselect

--- components/DisplayBox.jsx
+++ components/DisplayBox.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import FilteredMultiSelect from 'react-filtered-multiselect';
+import TokenInput from '../otherlibs/react-tokeninput/index';
+import {uniq} from 'lodash-node';
+import {without} from 'lodash-node';
 
 let CULTURE_SHIPS = [
   {id: 1, name: '5*Gelish-Oplule'},
@@ -9,57 +12,140 @@ let CULTURE_SHIPS = [
   {id: 250, name: 'Zoologist'}
 ]
 
-const MySelect = React.createClass({
+// const MySelect = React.createClass({
 
-  getInitialState() {
-    return {selectedShips: []}
+//   getInitialState() {
+//     return {selectedShips: []}
+//   },
+
+//   handleDeselect(index) {
+//     let selectedShips = this.state.selectedShips.slice()
+//     selectedShips.splice(index, 1)
+//     this.setState({selectedShips})
+//   },
+//   handleSelectionChange(selectedShips) {
+//     this.setState({selectedShips})
+//   },
+//   render() {
+//     let {selectedShips} = this.state
+//     return <div style={{width: '20%'}}>
+//       <br/>
+//       <FilteredMultiSelect
+//         onChange={this.handleSelectionChange}
+//         options={CULTURE_SHIPS}
+//         selectedOptions={selectedShips}
+//         textProp="name"
+//         valueProp="id"
+//         classNames={
+// 			   { button: 'btn btn-sm btn-block btn-warning glyphicon glyphicon-plus',
+// 			   	 buttonActive: 'FilteredMultiSelect__button--active',
+// 			   	 filter: 'form-control',
+// 			   	 select: 'form-control'}
+// 			  }
+// 		size='3'
+// 		buttonText=" Add Hint"
+// 		/>
+//       {selectedShips.length === 0 && <p>(Select a hint for this paragraph.)</p>}
+//       {selectedShips.length > 0 && <ul>
+//         {selectedShips.map((ship, i) => <li key={ship.id}>
+//           {`${ship.name} `}
+//           <button type="button" onClick={this.handleDeselect.bind(null, i)}>
+//             &times;
+//           </button>
+//         </li>)}
+//       </ul>}
+//     </div>
+//   }
+// })
+
+var DanSelect = React.createClass({
+  getInitialState: function() {
+    return {
+      selected: [],
+      options: {}
+    };
   },
 
-  handleDeselect(index) {
-    let selectedShips = this.state.selectedShips.slice()
-    selectedShips.splice(index, 1)
-    this.setState({selectedShips})
+  handleChange: function(value) {
+    this.setState({
+      selected: value
+    })
   },
-  handleSelectionChange(selectedShips) {
-    this.setState({selectedShips})
+
+  handleRemove: function(value) {
+    var selectedOptions = uniq(without(this.state.selected,value))
+    this.handleChange(selectedOptions)
   },
-  render() {
-    let {selectedShips} = this.state
-    return <div style={{width: '20%'}}>
-      <br/>
-      <FilteredMultiSelect
-        onChange={this.handleSelectionChange}
-        options={CULTURE_SHIPS}
-        selectedOptions={selectedShips}
-        textProp="name"
-        valueProp="id"
-        classNames={
-			   { button: 'btn btn-sm btn-block btn-warning glyphicon glyphicon-plus',
-			   	 buttonActive: 'FilteredMultiSelect__button--active',
-			   	 filter: 'form-control',
-			   	 select: 'form-control'}
-			  }
-		size='3'
-		buttonText=" Add Hint"
-		/>
-      {selectedShips.length === 0 && <p>(Select a hint for this paragraph.)</p>}
-      {selectedShips.length > 0 && <ul>
-        {selectedShips.map((ship, i) => <li key={ship.id}>
-          {`${ship.name} `}
-          <button type="button" onClick={this.handleDeselect.bind(null, i)}>
-            &times;
-          </button>
-        </li>)}
-      </ul>}
-    </div>
+
+  handleSelect: function(value, combobox) {
+    if(typeof value === 'string') {
+      value = {id: value, name: value};
+    }
+
+    var selected = uniq(this.state.selected.concat([value]))
+    this.setState({
+      selected: selected,
+      selectedToken: null
+    })
+
+    this.handleChange(selected)
+  },
+
+  handleInput: function(userInput) {
+    // this.setState({selectedStateId: null});
+    this.filterTags(userInput);
+  },
+
+  filterTags: function(userInput) {
+    if (userInput === '')
+      return this.setState({options: []});
+    var filter = new RegExp('^'+userInput, 'i');
+    this.setState({options: names.filter(function(state) {
+      return filter.test(state.name) || filter.test(state.id);
+    })});
+  },
+
+    renderComboboxOptions: function() {
+    return this.state.options.map(function(name) {
+      return (
+        <ComboboxOption
+          key={name.id}
+          value={name}
+        >{name.name}</ComboboxOption>
+      );
+    });
+  },
+
+  render: function() {
+    var selectedFlavors = this.state.selected.map(function(tag) {
+      return <li key={tag.id}>{tag.name}</li>
+    })
+
+
+    var options = this.state.options.length ?
+      this.renderComboboxOptions() : [];
+
+    return (
+      <div>
+        <TokenInput
+            onChange={this.handleChange}
+            onInput={this.handleInput}
+            onSelect={this.handleSelect}
+            onRemove={this.handleRemove}
+            selected={this.state.selected}
+			menuContent={options} 	/>
+
+        <h2>Selected</h2>
+        <ul>
+          {selectedFlavors}
+        </ul>
+      </div>
+    );
   }
 })
 
-<<<<<<< HEAD
 export const DisplayBox = React.createClass({
 
-=======
-const DisplayBox = React.createClass({
 	getDefaultProps() {
 		return {
 			backgroundColor: "#FFF",
@@ -68,7 +154,7 @@ const DisplayBox = React.createClass({
 			margin: "8px"
 		}
 	},
->>>>>>> refs/remotes/origin/master
+
 	render() {
 		return (
 			<div>
@@ -85,7 +171,7 @@ const DisplayBox = React.createClass({
 					> Remove</button>
 				</div>
 				<div>
-					<MySelect/>
+					<DanSelect/>
 				</div>	
 			</div>
 		);
