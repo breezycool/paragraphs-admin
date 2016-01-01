bdd29341686bc8f7065d2e248db050db4134f80f
breezycool <lachlankermode@live.com>
2016-01-01 18:13:28 +0000

    current working

--- redux/parseHTTP.js
+++ redux/parseHTTP.js
@@ -1,29 +1,84 @@
 import Promise from 'bluebird'
 import Parse from 'parse/node'
 
+import {newParagraph} from './paragraphsReducer'
+import {hints} from './hintsReducer'
+
 Parse.initialize(
   "tkaxaYALkFGuXoLebRyjGU6zxTV7Yswz7Y04zWG1",
   "KVCwYVJhND1pfaE1lu8tT1Pe3MStpeqHyaevzttj"
 );
 
-export const postStateToParse = (state) => {
-  return new Promise((resolve, reject) => {
-
-    let Paragraphs = Parse.Object.extend('Paragraphs')
-
-    
-
-    //TODO: implement Parse logic to send to server
-
-    resolve(state)
-  })
-}
+let Hint = Parse.Object.extend('Hint')
+let Paragraph = Parse.Object.extend('Paragraph')
 
 export const getStateFromParse = (state) => {
-    return new Promise((resolve, reject) => {
+  var allParagraphs = new Parse.Query(Paragraph)
+  var allHints = new Parse.Query(Hint)
 
-      //TODO: implement Parse logic to send to server
+  allHints.find().then((serverHints) => {
 
-      resolve(state)
+    // mess with serverHints
+
+    return [serverHints, allParagraphs.find()]
+  }).spread((serverHints, serverParagraphs) => {
+
+    // mess with hints and paragraphs
+    // create state via reducers (that take in initial state)
+    serverParagraphs.forEach((p) => {
+      // TODO: do somethign with hintsRefactord
+      newParagraph(p.objectId, p.badText, p.improvedText, hintsRefactord)
     })
+
+    state
+
+    return state
+  }).then((success) => {
+    // success
+  }, (error) => {
+    // error
+    return "ERROR: "+error
+  })
+
+}
+
+export const postStateToParse = (state) => {
+  // return new Promise((resolve, reject) => {
+  //
+  //   let hints = []
+  //   state.hints.forEach(h => {
+  //     let hint = new Hint()
+  //     hint.objectId = h.id
+  //     hint.text = h.text
+  //
+  //     hints.push(hint)
+  //   })
+  //
+  //   let paragraphs = []
+  //   state.paragraphs.forEach(p => {
+  //
+  //     let paragraph = new Paragraph()
+  //     paragraph.objectId = p.id
+  //     paragraph.badText = p.badText
+  //     paragraph.improvedText = p.improvedText
+  //     paragraph.hints = hintsAsParseObjects(p.hintTags)
+  //
+  //   })
+  //
+  //   state.paragraphs.map((p) => {
+  //     hintTags = p.hintTags.map(m => )
+  //     return {
+  //       badText: p.badText,
+  //       improvedText: p.improvedText,
+  //       hintTags: [
+  //
+  //       ]
+  //     }
+  //   })
+  //
+  //
+  //   //TODO: implement Parse logic to send to server
+  //
+  //   resolve(state)
+  // })
 }
